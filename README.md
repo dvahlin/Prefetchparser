8201af002f0ea6cfcba88c12b634eec316c4589c
dvahlin <39875016+dvahlin@users.noreply.github.com>
2023-08-14 21:33:15 +0200

    modified and adjusted for changes

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Prefetchparser
-Parser for Windows10 Prefetch files
+Parser for Windows10/11 Prefetch files
 
 Using libscca (https://github.com/libyal/libscca)
 
@@ -7,9 +7,38 @@ Script to parse output from pyscca
 
 Work in progress
 
+With PIP
+* pip install libscca-python
+
 Install libscca:
 * Download tar file from https://github.com/libyal/libscca/releases
 * Unpack with tar xvf <file>
 * run command: python3 setup.py install
 
+
+### Example usage and output
+
+```
+┌──(user㉿computer)-[~/prefetch]
+└─$ python3 parse.py -i Prefetch/POWERSHELL.EXE-59FC8F3D.pf
+EXECUTABLE NAME:        POWERSHELL.EXE 
+RUN COUNT:      26 
+LAST EXECUTED:  2023-08-08 21:00:21.801815
+
+
+HISTORY:
+Run 1:  2023-08-08 21:00:21.801815
+Run 2:  2023-08-08 20:44:44.421291
+Run 3:  2023-08-08 20:41:01.438052
+Run 4:  2023-08-08 20:14:10.854393
+Run 5:  2023-08-09 04:35:57.208765
+Run 6:  2023-08-08 05:26:15.977499
+Run 7:  2023-08-08 05:26:15.143587
+Run 8:  2023-08-08 05:26:05.973647
+```
+
+
+
 Dependencies identified: python3-dev python3-setuptools
+
+
--- parser.py
+++ parser.py
@@ -1,40 +1,40 @@
 import pyscca
-import os
-import sys
-import getopt
 import argparse
 
+def parse_prefetch(file_path):
+    with open(file_path, 'rb') as file_object:
+        scca_file = pyscca.file()
+        scca_file.open_file_object(file_object)
+
+        name = scca_file.get_executable_filename()
+        lastrun = scca_file.get_last_run_time(0)
+        history = scca_file.get_run_count()
+
+        # Print static output
+        print("EXECUTABLE NAME: \t{} \nRUN COUNT: \t{} \nLAST EXECUTED: \t{}".format(name, history, lastrun))
+        print("\n")
+
+        print("HISTORY:")
+        history = scca_file.get_run_count()
+        for run_count in range(history):
+            print("Run {}: \t{}".format(run_count + 1, scca_file.get_last_run_time(run_count)))
+
+        print("\n")
+
+        print("RESOURCES LOADED:")
+        for file_count in range(scca_file.get_number_of_filenames()):
+            print(scca_file.get_filename(file_count))
+
 def main():
-    parser = argparse.ArgumentParser(description='Prefetch parser for Windows 10')
-    parser.add_argument("-i", required=True, help="Prefetchfile to parse")
+    parser = argparse.ArgumentParser(description='Prefetch parser for Windows 10/11')
+    parser.add_argument("-i", required=True, help="Prefetch file to parse")
 
     args = parser.parse_args()
-    i = args.i
 
-    file_object = open(i, 'rb')
-    scca_file = pyscca.file()
-    scca_file.open_file_object(file_object)
-
-    name = scca_file.get_executable_filename()
-    lastrun = scca_file.get_last_run_time(0)
-    history = scca_file.get_run_count()
-    resources = scca_file.get_filename(0)
-    totalfilenames = scca_file.get_number_of_filenames()
-
-    # Print static output
-    print("EXECUTABLE NAME: \t{} \nRUN COUNT: \t{} \nLAST EXECUTED: \t{}".format(name,history,lastrun))
-
-    print("\n")
-
-    print("HISTORY:")
-    for run_count in range(scca_file.get_run_count()):
-        print(scca_file.get_last_run_time(run_count))
-
-    print("\n")
-
-    print("RESOURCES LOADED:")
-    for file_count in range(scca_file.get_number_of_filenames()):
-        print(scca_file.get_filename(file_count))
+    try:
+        parse_prefetch(args.i)
+    except Exception as e:
+        print(f"Error: {e}")
 
 if __name__ == '__main__':
     main()
